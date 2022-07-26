936d8668b68ab6e492d0e78752c7ab271f69fbf5
Eli-gav <108551274+Eli-gav@users.noreply.github.com>
2022-07-25 17:42:39 -0800

    Basic_JavaScript

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+
+
+var Sent1=("This is the beginning of the string"); //Defining my first variable//
+var Sent2=(" and this is the end of the string.");//Definging second variable//
+var A=("4"); //Defining variables for a expression//
+var B=("9");
+document.write(Sent1 + Sent2);//Concatenate variable 1 and 2// 
+window.alert("Welcome!");
+document.write(A*B); //print of varible A+B//
